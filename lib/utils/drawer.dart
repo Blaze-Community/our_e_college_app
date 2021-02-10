e70407cd35d52dbbdcb111a5b3dc7f89ab7bfa72
Harsh Shrivastava <Alexshri940@gmail.com>
2021-02-10 19:48:46 +0530

    added new type of drawer

--- lib/utils/drawer.dart
+++ lib/utils/drawer.dart
@@ -1,92 +1,166 @@
 import 'package:flutter/material.dart';
 
-class DrawerApp extends StatelessWidget {
+class DrawerApp extends StatefulWidget {
   @override
+  _DrawerAppState createState() => _DrawerAppState();
+}
+
+class _DrawerAppState extends State<DrawerApp> {
   Widget build(BuildContext context) {
-    return new Drawer(
-      child: ListView(
+    return new Container(
+      color: Colors.black87,
+      padding: EdgeInsets.only(top: 40, bottom: 40, left: 10),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          UserAccountsDrawerHeader(
-            accountName: Text('Draco'),
-            accountEmail: Text('klpd@gmail.com'),
-            currentAccountPicture: GestureDetector(
-              child: CircleAvatar(
-                backgroundColor: Colors.grey,
-                child: Icon(
-                  Icons.person,
-                  color: Colors.white,
-                ),
+          Row(children: [
+            CircleAvatar(
+              backgroundColor: Colors.grey,
+              child: Icon(
+                Icons.person,
+                color: Colors.white,
               ),
             ),
-            decoration: BoxDecoration(color: Colors.red),
-          ),
-          //Body
-          InkWell(
-            onTap: () {},
-            child: ListTile(
-              title: Text('Profile'),
-              leading: Icon(Icons.person),
+            SizedBox(
+              width: 10,
             ),
-          ),
-
-          InkWell(
-            onTap: () {},
-            child: ListTile(
-              title: Text('Reports'),
-              leading: Icon(Icons.assessment),
-            ),
-          ),
-
-          InkWell(
-            onTap: () {},
-            child: ListTile(
-              title: Text('Assigmnets'),
-              leading: Icon(Icons.assignment),
-            ),
-          ),
-
-          InkWell(
-            onTap: () {},
-            child: ListTile(
-              title: Text('Attendance'),
-              leading: Icon(Icons.bar_chart_rounded),
-            ),
-          ),
-
-          InkWell(
-            onTap: () {},
-            child: ListTile(
-              title: Text('Notice'),
-              leading: Icon(Icons.content_paste_sharp),
-            ),
-          ),
-
-          Divider(),
-          Expanded(
-              child: Container(
-            color: Colors.blueGrey,
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.end,
+            Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                InkWell(
-                  onTap: () {},
-                  child: ListTile(
-                    title: Text('Settings'),
-                    leading: Icon(Icons.settings),
-                  ),
-                ),
-                InkWell(
-                  onTap: () {},
-                  child: ListTile(
-                    title: Text('Help and Feedback'),
-                    leading: Icon(Icons.help, color: Colors.green),
-                  ),
-                ),
+                Text("harsh Shirvastava",
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold)),
+                Text("Active Status",
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold)),
               ],
-            ),
-          )),
+            )
+          ]),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Icon(
+                    Icons.person,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    'Profile',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Icon(Icons.assessment, color: Colors.white, size: 30),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    'Reports',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Icon(Icons.assignment, color: Colors.white, size: 30),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    'Assigmnets',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Icon(Icons.bar_chart_rounded, color: Colors.white, size: 30),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    'Attendance',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Icon(Icons.content_paste_sharp,
+                      color: Colors.white, size: 30),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    'Notice',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20),
+                  )
+                ],
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Icon(
+                Icons.settings,
+                color: Colors.white,
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Text('Settings',
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold)),
+              SizedBox(
+                width: 10,
+              ),
+              Container(
+                width: 2,
+                height: 20,
+                color: Colors.white,
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Text('Log Out',
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold)),
+            ],
+          ),
         ],
       ),
     );
