f7591125ae7a3ccb418df09f3c52a13151fcf54e
Harsh Shrivastava <Alexshri940@gmail.com>
2021-05-22 15:08:51 +0530

    added new timetable UI

--- lib/components/timetable.dart
+++ lib/components/timetable.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class TimeTable extends StatefulWidget {
   @override
@@ -7,216 +9,222 @@ class TimeTable extends StatefulWidget {
 }
 
 class _TimeTableState extends State<TimeTable> {
-  String selected = "MONDAY";
+  CalendarController _controller;
+  @override
+  void initState() {
+    super.initState();
+    _controller = CalendarController();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-            margin: EdgeInsets.symmetric(vertical: 10.0),
-            height: double.infinity,
-            child: Column(children: [
-              Container(
-                  height: 40,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: <Widget>[
-                      Container(
-                          width: 150.0,
-                          decoration:
-                              BoxDecoration(color: Colors.white, boxShadow: [
-                            BoxShadow(
-                              color: Colors.black,
-                              blurRadius: 1.0,
-                            )
-                          ]),
-                          child: ListTile(
-                            title: Text(
-                              "Monday",
-                              style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: selected == "MONDAY"
-                                      ? FontWeight.bold
-                                      : null),
-                              textAlign: TextAlign.center,
+        body: Stack(
+      children: [
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+          alignment: Alignment.topCenter,
+          color: Color(0xFFF0F0F0),
+          height: MediaQuery.of(context).size.height,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Icon(Icons.calendar_today, color: Colors.grey),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  RichText(
+                    text: TextSpan(
+                        text: "May",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Color(0XFF263064),
+                          fontSize: 22,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: " 2021",
+                            style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 16,
                             ),
-                            onTap: () {
-                              setState(() {
-                                selected = "MONDAY";
-                              });
-                            },
-                          )),
-                      Container(
-                          width: 150.0,
-                          decoration:
-                              BoxDecoration(color: Colors.white, boxShadow: [
-                            BoxShadow(
-                              color: Colors.black,
-                              blurRadius: 1.0,
-                            )
-                          ]),
-                          child: ListTile(
-                            title: Text(
-                              "Tuesday",
-                              style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: selected == "TUESDAY"
-                                      ? FontWeight.bold
-                                      : null),
-                              textAlign: TextAlign.center,
-                            ),
-                            onTap: () {
-                              setState(() {
-                                selected = "TUESDAY";
-                              });
-                            },
-                          )),
-                      Container(
-                          width: 150.0,
-                          decoration:
-                              BoxDecoration(color: Colors.white, boxShadow: [
-                            BoxShadow(
-                              color: Colors.black,
-                              blurRadius: 1.0,
-                            )
-                          ]),
-                          child: ListTile(
-                            title: Text(
-                              "Wednesday",
-                              style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: selected == "WEDNESDAY"
-                                      ? FontWeight.bold
-                                      : null),
-                              textAlign: TextAlign.center,
-                            ),
-                            onTap: () {
-                              setState(() {
-                                selected = "WEDNESDAY";
-                              });
-                            },
-                          )),
-                      Container(
-                          width: 150.0,
-                          decoration:
-                              BoxDecoration(color: Colors.white, boxShadow: [
-                            BoxShadow(
-                              color: Colors.black,
-                              blurRadius: 1.0,
-                            )
-                          ]),
-                          child: ListTile(
-                            title: Text(
-                              "Thursday",
-                              style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: selected == "THURSDAY"
-                                      ? FontWeight.bold
-                                      : null),
-                              textAlign: TextAlign.center,
-                            ),
-                            onTap: () {
-                              setState(() {
-                                selected = "THURSDAY";
-                              });
-                            },
-                          )),
-                      Container(
-                          width: 150.0,
-                          decoration:
-                              BoxDecoration(color: Colors.white, boxShadow: [
-                            BoxShadow(
-                              color: Colors.black,
-                              blurRadius: 1.0,
-                            )
-                          ]),
-                          child: ListTile(
-                            title: Text(
-                              "Friday",
-                              style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: selected == "FRIDAY"
-                                      ? FontWeight.bold
-                                      : null),
-                              textAlign: TextAlign.center,
-                            ),
-                            onTap: () {
-                              setState(() {
-                                selected = "FRIDAY";
-                              });
-                            },
-                          )),
-                    ],
-                  )),
-              TimeTableBox(
-                  subject: "English", room: "102", time: "9:00am - 9:55am"),
-              TimeTableBox(
-                  subject: "Godzilla", room: "105", time: "10:00am - 10:55am"),
-              TimeTableBox(
-                  subject: "Maths", room: "101", time: "12:00pm - 12:55pm"),
-            ])));
+                          ),
+                        ]),
+                  ),
+                ],
+              ),
+              Text(
+                "CSE Batch - A",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Color(0XFF3E3993),
+                    fontSize: 16),
+              )
+            ],
+          ),
+        ),
+        Positioned(
+          top: 100,
+          child: Container(
+            height: MediaQuery.of(context).size.height - 100,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: Column(
+              children: [
+                SingleChildScrollView(
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                      SizedBox(
+                        height: 15,
+                      ),
+                      TableCalendar(
+                        calendarController: _controller,
+                        initialCalendarFormat: CalendarFormat.week,
+                        headerVisible: false,
+                        // calendarStyle: CalendarStyle(todayStyle: ),
+                      ),
+                    ])),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        buildTaskListItem("DSA", "07:00"),
+                        buildTaskListItem("DPPL", "08:00"),
+                        buildTaskListItem("ITW2", "09:00"),
+                        buildTaskListItem("SE", "10:00"),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        )
+      ],
+    ));
   }
-}
 
-class TimeTableBox extends StatelessWidget {
-  final String subject;
-  final String room;
-  final String time;
-
-  TimeTableBox({this.subject, this.room, this.time});
-
-  @override
-  Widget build(BuildContext context) {
+  Container buildTaskListItem(String classSubject, String classTime) {
     return Container(
-      width: double.infinity,
-      height: 100,
-      decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(20),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black,
-              blurRadius: 5.0,
-              spreadRadius: 2.0,
-            )
-          ]),
+      margin: EdgeInsets.only(bottom: 25),
       child: Column(
         children: [
-          Text(
-            subject,
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          Row(
+            children: [
+              Container(
+                width: 15,
+                height: 10,
+                decoration: BoxDecoration(
+                    color: Colors.orange,
+                    borderRadius: BorderRadius.horizontal(
+                      right: Radius.circular(5),
+                    )),
+              ),
+              SizedBox(
+                width: 15,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width - 60,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                          text: classTime,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                          children: [
+                            TextSpan(
+                              text: " AM",
+                              style: TextStyle(
+                                fontWeight: FontWeight.normal,
+                                color: Colors.grey,
+                              ),
+                            )
+                          ]),
+                    ),
+                    Text(
+                      "1 h 00 min",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.grey,
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
           ),
           SizedBox(
             height: 10,
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Time",
-                    style: TextStyle(fontWeight: FontWeight.bold),
+          Container(
+            height: 120,
+            width: double.infinity,
+            decoration: BoxDecoration(
+                border: Border.all(width: 1, color: Colors.grey[300]),
+                borderRadius: BorderRadius.circular(20)),
+            margin: EdgeInsets.only(right: 10, left: 30),
+            padding: EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  classSubject,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15,
                   ),
-                  Text(time),
-                ],
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Room",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  Text(room),
-                ],
-              ),
-            ],
-          ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Icon(
+                      Icons.location_on,
+                      size: 20,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Room",
+                          style: TextStyle(
+                            fontSize: 15,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          "Room C1, 1st floor",
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
         ],
       ),
-      margin: EdgeInsets.only(top: 15, left: 15, right: 15),
-      padding: EdgeInsets.only(top: 15),
     );
   }
 }
