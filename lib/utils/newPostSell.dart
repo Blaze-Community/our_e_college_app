797cf417468d5c05443916764ddb61f88d896d3d
Aa@02102000 <ashishsonam00@gmail.com>
2021-05-26 02:33:03 +0530

    new post feature

--- lib/utils/newPostSell.dart
+++ lib/utils/newPostSell.dart
@@ -6,6 +6,7 @@ class NewPostSell extends StatefulWidget {
 }
 
 class _NewPostSellState extends State<NewPostSell> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,118 +36,169 @@ class _NewPostSellState extends State<NewPostSell> {
           ],
         ),
         body: ListView(children: [
-          Stack(children: [
-            Container(
-                height: MediaQuery.of(context).size.height - 82.0,
-                width: MediaQuery.of(context).size.width,
-                color: Colors.transparent),
-            Positioned(
-                top: 75.0,
-                child: Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(50.0),
-                          topRight: Radius.circular(50.0),
-                        ),
-                        color: Colors.white),
-                    height: MediaQuery.of(context).size.height - 100.0,
-                    width: MediaQuery.of(context).size.width)),
-            // SizedBox(height: 40),
-            // Positioned(
-            //     top: 30.0,
-            //     left: (MediaQuery.of(context).size.width / 2) - 100.0,
-            //     child: Hero(
-            //         tag: widget.heroTag,
-            //         child: Container(
-            //             decoration: BoxDecoration(
-            //                 image: DecorationImage(
-            //                     image: AssetImage(widget.heroTag),
-            //                     fit: BoxFit.cover)),
-            //             height: 200.0,
-            //             width: 200.0))),
-            // Positioned(
-            //     top: 400.0,
-            //     left: 25.0,
-            //     right: 25.0,
-            //     child: Column(
-            //       crossAxisAlignment: CrossAxisAlignment.start,
-            //       children: <Widget>[
-            //         Text(widget.itemName,
-            //             style: TextStyle(
-            //                 fontFamily: 'Montserrat',
-            //                 fontSize: 22.0,
-            //                 fontWeight: FontWeight.bold)),
-            //         SizedBox(height: 20.0),
-            //         Row(
-            //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //           children: <Widget>[
-            //             Text('Rs. ' + widget.itemPrice,
-            //                 style: TextStyle(
-            //                     fontFamily: 'Montserrat',
-            //                     fontSize: 20.0,
-            //                     color: Colors.grey)),
-                        // Container(height: 25.0, color: Colors.grey, width: 1.0),
-                        // Container(
-                        //   width: 125.0,
-                        //   height: 40.0,
-                        //   decoration: BoxDecoration(
-                        //       borderRadius: BorderRadius.circular(17.0),
-                        //       color: Color(0xFF7A9BEE)),
-                        //   child: Row(
-                        //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        //     children: <Widget>[
-                        //       InkWell(
-                        //         onTap: () {},
-                        //         child: Container(
-                        //           height: 25.0,
-                        //           width: 25.0,
-                        //           decoration: BoxDecoration(
-                        //               borderRadius: BorderRadius.circular(7.0),
-                        //               color: Color(0xFF7A9BEE)),
-                        //           child: Center(
-                        //             child: Icon(
-                        //               Icons.remove,
-                        //               color: Colors.white,
-                        //               size: 20.0,
-                        //             ),
-                        //           ),
-                        //         ),
-                        //       ),
-                        //       Text('2',
-                        //           style: TextStyle(
-                        //               color: Colors.white,
-                        //               fontFamily: 'Montserrat',
-                        //               fontSize: 15.0)),
-                        //       InkWell(
-                        //         onTap: () {},
-                        //         child: Container(
-                        //           height: 25.0,
-                        //           width: 25.0,
-                        //           decoration: BoxDecoration(
-                        //               borderRadius: BorderRadius.circular(7.0),
-                        //               color: Colors.white),
-                        //           child: Center(
-                        //             child: Icon(
-                        //               Icons.add,
-                        //               color: Color(0xFF7A9BEE),
-                        //               size: 20.0,
-                        //             ),
-                        //           ),
-                        //         ),
-                        //       )
-                        //     ],
-                        //   ),
-                        // )
-                      ],
+                Stack(children: [
+                    Container(
+                        height: MediaQuery.of(context).size.height - 82.0,
+                        width: MediaQuery.of(context).size.width,
+                        color: Colors.transparent),
+                    Positioned(
+                        top: 75.0,
+                        child: Container(
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.only(
+                                      topLeft: Radius.circular(50.0),
+                                      topRight: Radius.circular(50.0),
+                                    ),
+                                    color: Colors.white),
+                                height: MediaQuery.of(context).size.height - 100.0,
+                                width: MediaQuery.of(context).size.width
+                              )
                     ),
-                    // SizedBox(height: 40.0),
-                    // Container(
-                    //   // height: 200.0,
-                    //   child: _buildContactDetails(
-                    //       'Ashish Sonam', 'C-21', '8210864839'),
-                    // ),
-                    // SizedBox(height: 20.0),
-                  ],
-                ));
+                    SizedBox(height: 40),
+                    Positioned(
+                      top: 100,
+                      left: 25.0,
+                      right: 25.0,
+                      child: Column(
+                        children: [
+                          Text('Item Details',
+                            style: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontSize: 16.0,
+                                color: Colors.grey)),
+                          SizedBox(height: 5),
+                          Container(height: 1.0, color: Colors.deepOrange, width: 50.0),
+                          SizedBox(height: 20),
+                          Container(
+                            padding: EdgeInsets.fromLTRB(10,2,10,2),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              border: Border.all(color: Colors.deepPurple)
+                            ),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                labelText: "Item Name",
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          Container(
+                            padding: EdgeInsets.fromLTRB(10,2,10,2),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              border: Border.all(color: Colors.deepPurple)
+                            ),
+                            child: TextField(
+                              keyboardType: TextInputType.number,
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                labelText: "Price",
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          OutlinedButton(
+                            onPressed: () {},
+                            
+                            child: Padding(
+                              padding: EdgeInsets.all(15),
+                              
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(Icons.upload, size: 25),
+                                    SizedBox(width: 10),
+                                    Text(
+                                      "Upload Image",
+                                      style: TextStyle(
+                                        fontSize: 15,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                            )
+                          ),
+                          SizedBox(height: 40),
+                          Text('Contact Details',
+                            style: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontSize: 16.0,
+                                color: Colors.grey)),
+                          SizedBox(height: 5),
+                          Container(height: 1.0, color: Colors.deepOrange, width: 50.0),
+                          SizedBox(height: 20),
+                          Container(
+                            padding: EdgeInsets.fromLTRB(10,2,10,2),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              border: Border.all(color: Colors.deepPurple)
+                            ),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                labelText: "Name",
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          Container(
+                            padding: EdgeInsets.fromLTRB(10,2,10,2),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              border: Border.all(color: Colors.deepPurple)
+                            ),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                labelText: "Room No.",
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          Container(
+                            padding: EdgeInsets.fromLTRB(10,2,10,2),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              border: Border.all(color: Colors.deepPurple)
+                            ),
+                            child: TextField(
+                              keyboardType: TextInputType.number,
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                labelText: "Contact No.",
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: Padding(
+                              padding: EdgeInsets.all(15),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(Icons.publish, size: 25),
+                                    SizedBox(width: 10),
+                                    Text(
+                                      "Submit",
+                                      style: TextStyle(
+                                        fontSize: 15,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                            )
+                          ),
+                        ],
+                      )
+                    )
+                  ]
+                )
+              ]
+            )
+        );
   }
 }
