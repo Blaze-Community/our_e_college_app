639a466aa94b6d89008b5fcdbdf3586a3e59b908
Harsh Shrivastava <Alexshri940@gmail.com>
2021-05-26 11:51:57 +0530

    finally everything done with timetable

--- lib/components/timetable.dart
+++ lib/components/timetable.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:our_e_college_app/utils/timetableitems.dart';
 import 'package:table_calendar/table_calendar.dart';
 // import 'package:shared_preferences/shared_preferences.dart';
 
@@ -19,207 +20,125 @@ class _TimeTableState extends State<TimeTable> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          title: Text("TimeTable"),
+        ),
         body: Stack(
-      children: [
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-          alignment: Alignment.topCenter,
-          color: Color(0xFFF0F0F0),
-          height: MediaQuery.of(context).size.height,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+              alignment: Alignment.topCenter,
+              color: Color(0xFFF0F0F0),
+              height: MediaQuery.of(context).size.height,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Icon(Icons.calendar_today, color: Colors.grey),
-                  SizedBox(
-                    width: 15,
-                  ),
-                  RichText(
-                    text: TextSpan(
-                        text: "May",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Color(0XFF263064),
-                          fontSize: 22,
-                        ),
-                        children: [
-                          TextSpan(
-                            text: " 2021",
+                  Row(
+                    children: [
+                      Icon(Icons.calendar_today, color: Colors.grey),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      RichText(
+                        text: TextSpan(
+                            text: "May",
                             style: TextStyle(
-                              fontWeight: FontWeight.normal,
-                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0XFF263064),
+                              fontSize: 22,
                             ),
-                          ),
-                        ]),
+                            children: [
+                              TextSpan(
+                                text: " 2021",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.normal,
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ]),
+                      ),
+                    ],
                   ),
+                  Text(
+                    "CSE Batch - A",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Color(0XFF3E3993),
+                        fontSize: 16),
+                  )
                 ],
               ),
-              Text(
-                "CSE Batch - A",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Color(0XFF3E3993),
-                    fontSize: 16),
-              )
-            ],
-          ),
-        ),
-        Positioned(
-          top: 100,
-          child: Container(
-            height: MediaQuery.of(context).size.height - 220,
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(30),
             ),
-            child: Column(
-              children: [
-                SingleChildScrollView(
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                      SizedBox(
-                        height: 15,
-                      ),
-                      TableCalendar(
-                        calendarController: _controller,
-                        initialCalendarFormat: CalendarFormat.week,
-                        headerVisible: false,
-                        // calendarStyle: CalendarStyle(todayStyle: ),
-                      ),
-                    ])),
-                Expanded(
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        buildTaskListItem("DSA", "07:00"),
-                        buildTaskListItem("DPPL", "08:00"),
-                        buildTaskListItem("ITW2", "09:00"),
-                        buildTaskListItem("SE", "10:00"),
-                        buildTaskListItem("SE", "10:00"),
-                        buildTaskListItem("SE", "10:00"),
-                      ],
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
-        )
-      ],
-    ));
-  }
-
-  Container buildTaskListItem(String classSubject, String classTime) {
-    return Container(
-      margin: EdgeInsets.only(bottom: 25),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Container(
-                width: 15,
-                height: 10,
+            Positioned(
+              top: 100,
+              child: Container(
+                height: MediaQuery.of(context).size.height - 220,
+                width: MediaQuery.of(context).size.width,
                 decoration: BoxDecoration(
-                    color: Colors.orange,
-                    borderRadius: BorderRadius.horizontal(
-                      right: Radius.circular(5),
-                    )),
-              ),
-              SizedBox(
-                width: 15,
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width - 60,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30)),
+                ),
+                child: Column(
                   children: [
-                    RichText(
-                      text: TextSpan(
-                          text: classTime,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
+                    SingleChildScrollView(
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                          SizedBox(
+                            height: 15,
                           ),
+                          TableCalendar(
+                            calendarController: _controller,
+                            initialCalendarFormat: CalendarFormat.week,
+                            headerVisible: false,
+                            // calendarStyle: CalendarStyle(todayStyle: ),
+                          ),
+                        ])),
+                    Expanded(
+                      child: SingleChildScrollView(
+                        child: Column(
                           children: [
-                            TextSpan(
-                              text: " AM",
-                              style: TextStyle(
-                                fontWeight: FontWeight.normal,
-                                color: Colors.grey,
-                              ),
-                            )
-                          ]),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Container(
-            height: 120,
-            width: double.infinity,
-            decoration: BoxDecoration(
-                border: Border.all(width: 1, color: Colors.grey[300]),
-                borderRadius: BorderRadius.circular(20)),
-            margin: EdgeInsets.only(right: 10, left: 30),
-            padding: EdgeInsets.all(20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  classSubject,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 15,
-                  ),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Icon(
-                      Icons.location_on,
-                      size: 20,
-                    ),
-                    SizedBox(
-                      width: 5,
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Room",
-                          style: TextStyle(
-                            fontSize: 15,
-                          ),
+                            TimeTableItems(
+                              subject: "Software Engenring",
+                              time: "07:00",
+                              room: "class 401",
+                            ),
+                            TimeTableItems(
+                              subject: "Operating System",
+                              time: "08:00",
+                              room: "class 401",
+                            ),
+                            TimeTableItems(
+                              subject: "Object Oriented Programming",
+                              time: "09:00",
+                              room: "class 401",
+                            ),
+                            TimeTableItems(
+                              subject: "Data Structure",
+                              time: "11:00",
+                              room: "class 401",
+                            ),
+                            TimeTableItems(
+                              subject: "Digital Signals",
+                              time: "13:00",
+                              room: "class 407",
+                            ),
+                            TimeTableItems(
+                              subject: "Cloud computing",
+                              time: "15:00",
+                              room: "class 404",
+                            ),
+                          ],
                         ),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        Text(
-                          "Room C1, 1st floor",
-                          style: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 12,
-                          ),
-                        ),
-                      ],
+                      ),
                     )
                   ],
-                )
-              ],
-            ),
-          )
-        ],
-      ),
-    );
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
---
+++ lib/utils/timetableitems.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class TimeTableItems extends StatelessWidget {
+  String subject;
+  String time;
+  String room;
+
+  TimeTableItems({this.subject, this.time, this.room});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 25),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                width: 15,
+                height: 10,
+                decoration: BoxDecoration(
+                    color: Colors.orange,
+                    borderRadius: BorderRadius.horizontal(
+                      right: Radius.circular(5),
+                    )),
+              ),
+              SizedBox(
+                width: 15,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width - 60,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                        text: this.time,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            height: 120,
+            width: double.infinity,
+            decoration: BoxDecoration(
+                border: Border.all(width: 1, color: Colors.grey[300]),
+                borderRadius: BorderRadius.circular(20)),
+            margin: EdgeInsets.only(right: 10, left: 30),
+            padding: EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  this.subject,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15,
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Icon(
+                      Icons.location_on,
+                      size: 20,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Room",
+                          style: TextStyle(
+                            fontSize: 15,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          this.room,
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
