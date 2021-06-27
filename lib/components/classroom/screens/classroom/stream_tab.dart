6e2792f186ea200d602542030029cc4a94af096a
Harsh Shrivastava <Alexshri940@gmail.com>
2021-06-27 22:35:22 +0530

    added stream feature

--- lib/components/classroom/screens/classroom/stream_tab.dart
+++ lib/components/classroom/screens/classroom/stream_tab.dart
@@ -1,17 +1,45 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:our_e_college_app/components/classroom/widgets/comment_composer.dart';
+import 'package:our_e_college_app/global.dart' as Global;
+import 'package:our_e_college_app/components/classroom/classroom_helper.dart';
+import 'package:http/http.dart' as http;
+import 'package:simple_moment/simple_moment.dart';
 
 class StreamTab extends StatefulWidget {
-  AssetImage bannerImg;
-  String className;
+  final AssetImage bannerImg;
+  final String className;
+  final classDetails;
 
-  StreamTab({this.bannerImg, this.className});
+  StreamTab({this.bannerImg, this.className, this.classDetails});
 
   @override
   _StreamTabState createState() => _StreamTabState();
 }
 
 class _StreamTabState extends State<StreamTab> {
+  final message_for_class = TextEditingController();
+  uploadMessage() async {
+    var url = Uri.parse('http://localhost:5000/api/uploadMessage');
+    await http.post(url, body: {
+      "classId": widget.classDetails["_id"],
+      "message": message_for_class.text
+    }).then((response) {
+      ClassRoomHelper.shared.fetchClassInfo(widget.classDetails["_id"]);
+      final responseJson = json.decode(response.body);
+      message_for_class.text = "";
+      print(responseJson);
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    ClassRoomHelper.shared.fetchClassInfo(widget.classDetails["_id"]);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -39,27 +67,57 @@ class _StreamTabState extends State<StreamTab> {
             ],
           ),
         ),
-        Container(
+        if (Global.user != "Student")
+          Container(
             margin: EdgeInsets.symmetric(horizontal: 15),
-            height: 60,
             decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(5),
-                boxShadow: [BoxShadow(color: Colors.black26, blurRadius: 3)]),
-            child: Row(
-              children: [
-                SizedBox(width: 10),
-                CircleAvatar(
-                  backgroundImage: AssetImage('assets/cat3.jpg'),
-                ),
-                SizedBox(width: 10),
-                Text(
-                  "Share with your class...",
-                  style: TextStyle(color: Colors.grey),
-                )
-              ],
-            )),
-        CommentComposer(),
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(5),
+            ),
+            child: TextFormField(
+                controller: message_for_class,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Send Message",
+                  suffixIcon: InkWell(
+                      child: Icon(
+                        Icons.send,
+                        color: Colors.deepOrange,
+                      ),
+                      onTap: () {
+                        print("send message");
+                        setState(() {
+                          ClassRoomHelper.loading = true;
+                        });
+                        uploadMessage();
+                      }),
+                )),
+          ),
+        Expanded(
+          child: StreamBuilder(
+            stream: ClassRoomStreamControllerHelper.shared.classInfostream,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.active) {
+                if (snapshot.hasData) {
+                  final List items = snapshot.data["classInfo"]["messages"];
+                  return ListView.builder(
+                    itemCount: items.length,
+                    itemBuilder: (BuildContext ctxt, int i) {
+                      return CommentComposer(
+                        date: Moment.parse(items[i]["createdAt"])
+                            .format('dd-MM-yyyy'),
+                        message: items[i]["message"],
+                      );
+                    },
+                  );
+                }
+              }
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+          ),
+        )
       ],
     );
   }
--- lib/components/classroom/widgets/comment_composer.dart
+++ lib/components/classroom/widgets/comment_composer.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:our_e_college_app/components/classroom/data/comments.dart';
+import 'package:our_e_college_app/global.dart' as Global;
+import 'package:our_e_college_app/components/classroom/classroom_helper.dart';
 
 class CommentComposer extends StatefulWidget {
+  final String message;
+  final String date;
+  CommentComposer({this.date, this.message});
   @override
   _CommentComposerState createState() => _CommentComposerState();
 }
@@ -10,76 +15,55 @@ class CommentComposer extends StatefulWidget {
 class _CommentComposerState extends State<CommentComposer> {
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-        child: ListView.builder(
-            itemCount: commentList.length,
-            itemBuilder: (context, int index) {
-              return Container(
-                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(5),
-                      boxShadow: [
-                        BoxShadow(color: Colors.black26, blurRadius: 0)
-                      ]),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            children: [
-                              SizedBox(width: 10),
-                              CircleAvatar(
-                                backgroundImage:
-                                    AssetImage("${commentList[index].userDp}"),
-                              ),
-                              SizedBox(width: 10),
-                              Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      commentList[index].userName,
-                                      style: TextStyle(),
-                                    ),
-                                    Text(
-                                      commentList[index].date,
-                                      style: TextStyle(color: Colors.grey),
-                                    ),
-                                  ]),
-                            ],
-                          ),
-                          IconButton(
-                            icon: Icon(
-                              Icons.more_vert,
-                              color: Colors.grey,
-                            ),
-                            onPressed: () {},
-                          )
-                        ],
-                      ),
-                      Container(
-                          width: MediaQuery.of(context).size.width - 40,
-                          margin:
-                              EdgeInsets.only(left: 15, top: 15, bottom: 10),
-                          child: Text(commentList[index].comment)),
-                      Container(
-                        alignment: Alignment.centerLeft,
-                        height: 40,
-                        width: MediaQuery.of(context).size.width - 30,
-                        decoration: BoxDecoration(
-                            border: Border(
-                                top: BorderSide(
-                                    width: 1, color: Colors.grey[300]))),
-                        child: Text(
-                          "     Add class comment",
-                          style: TextStyle(fontSize: 12, color: Colors.grey),
-                        ),
-                      )
-                    ],
-                  ));
-            }));
+    return Container(
+        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+        padding: EdgeInsets.only(left: 15, right: 15),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(5),
+          border: Border.all(color: Colors.deepOrange),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 8,
+            ),
+            Row(
+              children: [
+                Expanded(
+                    child: Text(
+                  "Dated : ${widget.date}",
+                  style: TextStyle(color: Colors.black),
+                )),
+                if (Global.user != "Student")
+                  IconButton(
+                    icon: Icon(
+                      Icons.delete,
+                      color: Colors.grey,
+                    ),
+                    onPressed: () {},
+                  )
+              ],
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Text(
+              "Message - ",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 15,
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Container(
+                width: MediaQuery.of(context).size.width - 40,
+                margin: EdgeInsets.only(bottom: 10),
+                child: Text(widget.message)),
+          ],
+        ));
   }
 }
